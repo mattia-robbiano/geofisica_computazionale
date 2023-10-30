8b0731a457643f3519f3fee2744d6c3b11bcf7e9
Mattia Robbiano <120450443+mattia-robbiano@users.noreply.github.com>
2023-10-30 11:09:05 +0000

    implementata interpolazione lineare in radiation.f90

--- 2023-10-25-assignment/radiation.F90
+++ 2023-10-25-assignment/radiation.F90
@@ -2,11 +2,11 @@ PROGRAM RADIATION
    IMPLICIT NONE
 
    CHARACTER(LEN=100) :: FILE_INPUT='radiation.dat', FILE_OUTPUT='results.dat', TempChar
-   INTEGER :: IO, NumeroRighe=0, Riga=1, i=0,j=0
+   INTEGER :: IO, NumeroRighe=0, Riga=1, i=0,j=0, k=0
    REAL, ALLOCATABLE, DIMENSION(:,:) :: Dati
-   REAL :: TotalShortwave=0, TotalInfrared=0
+   REAL :: TotalShortwave=0, TotalInfrared=0, m=0, q=0
    INTEGER, DIMENSION(6) :: DataBuffer
-   LOGICAL :: DataCheck, ESISTE 
+   LOGICAL :: DataCheck, ESISTE
 
    WRITE(*,*) 'Fornire file input: '
    READ(*,*) FILE_INPUT
@@ -40,7 +40,8 @@ PROGRAM RADIATION
    END DO
 
    !ALLOCAZIONE MATRICE
-   ALLOCATE(Dati(NumeroRighe, 7))
+   ALLOCATE(Dati(NumeroRighe, 7), STAT=IO)
+   IF (IO/=0) STOP 'Errore allocazione matrice'
 
    !RITORNO ALL'INIZIO DATI
    REWIND(20)
@@ -56,7 +57,7 @@ PROGRAM RADIATION
    Riga = 1
    DO Riga = 1, NumeroRighe
 
-!CONTROLLO DATA
+      !CONTROLLO SE E' CAMBIATO IL GIORNO
       DataCheck = .TRUE.  ! Initialize DataCheck to TRUE
       i=1
       DO i = 1, 3
@@ -66,27 +67,40 @@ PROGRAM RADIATION
          END IF
       END DO
 
-!SE IL DATO E NULLO INTERPOLO tra i primi due valori non nulli
+      !SE IL DATO E NULLO INTERPOLO tra i primi due valori non nulli
       i=Riga
       j=Riga
-      IF(DataCheck .AND. Dati(Riga,7)<-998.0) THEN
+      interpolazione: IF(DataCheck .AND. Dati(Riga,7)<-998.0) THEN
 
-            DO
-                IF(Dati(i,7)>-998.0) EXIT
-                i=i-1
-            END DO
+         !CERCO PRIMO VALORE NON NULLO PRECEDENTE
+         DO
+            IF(Dati(i,7)>-998.0) EXIT
+            i=i-1
+         END DO
+         !CERCO PRIMO VALORE NON NULLO SUCCESSIVO
+         DO
+            IF(Dati(j,7)>-998.0) EXIT
+            j=j+1
+         END DO
+         !SE HO PIU DI 4 VALORI NULLI ESCO
+         IF(j-i>5) THEN
+            WRITE(*,*) 'Troppi valori nulli tra', i+1+1, j-1+1 !TENGO CONTO CHE HO UNA RIGA DI INTESTAZIONE PER LETTURA A MANO DA FILE
+            STOP
+         END IF
 
-            DO
-                IF(Dati(j,7)>-998.0) EXIT
-                j=j+1
-            END DO
+         !ALTRIMENTI INTERPOLAZIONE LINEARE
+         WRITE(*,101) 'Interpolazione tra', INT(Dati(i+1,1:6)), 'e', INT(Dati(j-1,1:6))
+         m = (Dati(j,7)-Dati(i,7))/(j-i)
+         q = Dati(i,7)-m*i
+         k=i+1
+         DO
+            Dati(k,7) = m*k+q
+            WRITE(*,*) k, Dati(k,7)
+            IF(k==j-1) EXIT
+            k=k+1
+         END DO
 
-            IF(j-i>5) THEN 
-                WRITE(*,*) 'Troppi valori nulli tra', i+1+1, j-1+1 !TENGO CONTO CHE HO UNA RIGA DI INTESTAZIONE
-                STOP
-            END IF
-            Dati(Riga,7) = (Dati(i,7)+Dati(j,7))/2.0
-      END IF
+      END IF interpolazione
 
       IF(DataCheck .AND. Dati(Riga,7)>=0.0) THEN
          TotalShortwave = TotalShortwave + Dati(Riga,7)
@@ -106,5 +120,8 @@ PROGRAM RADIATION
    CLOSE(20)
    CLOSE(21)
 100 FORMAT(I2,1X,I2,1X,I4,1X,F10.2,1X,F10.2)
+101 FORMAT(A,1X,I2,'/',I2,'/',I4,1X,I2,':',I2,':',I2,1X,A,1X,I2,'/',I2,'/',I4,1X,I2,':',I2,':',I2)
+
+   DEALLOCATE(Dati)
 
 END PROGRAM RADIATION
