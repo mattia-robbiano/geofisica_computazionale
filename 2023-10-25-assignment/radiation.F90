c5e56d858cf4a70624556e5702d3027dd41479c4
Mattia Robbiano <120450443+mattia-robbiano@users.noreply.github.com>
2023-11-04 17:04:00 +0000

    versione finale radiation

--- 2023-10-25-assignment/radiation.F90
+++ 2023-10-25-assignment/radiation.F90
@@ -1,22 +1,26 @@
 PROGRAM RADIATION
    IMPLICIT NONE
 
+   INTEGER, PARAMETER :: DBL = SELECTED_REAL_KIND(p=13)
    CHARACTER(LEN=100) :: FILE_INPUT='radiation.dat', FILE_OUTPUT='results.dat', TempChar
    INTEGER :: IO, NumeroRighe=0, Riga=1, i=0,j=0, k=0
-   REAL, ALLOCATABLE, DIMENSION(:,:) :: Dati
-   REAL :: TotalShortwave=0, TotalInfrared=0, m=0, q=0
+   REAL(KIND=DBL), ALLOCATABLE, DIMENSION(:,:) :: Dati
+   REAL(KIND=DBL) :: TotalShortwave=0, TotalInfrared=0, m=0, q=0
    INTEGER, DIMENSION(6) :: DataBuffer
    LOGICAL :: DataCheck, ESISTE
 
+   !Chiedi all'utente file input
    WRITE(*,*) 'Fornire file input: '
    READ(*,*) FILE_INPUT
    OPEN(UNIT=20,FILE=FILE_INPUT,STATUS='OLD',ACTION='READ',IOSTAT=IO)
    IF (IO/=0) STOP 'Errore apertura file input'
 
+   !Chiedi all'utente file output
    WRITE(*,*) 'Fornire file output: '
    READ(*,*) FILE_OUTPUT
    INQUIRE(FILE=FILE_OUTPUT,EXIST=ESISTE)
 
+   !CONTROLLO SE ESISTE GIA
    IF (ESISTE) THEN
       WRITE(*,*) 'Il file esiste gia, sovrascriverlo? (y/n)'
       READ(*,*) TempChar
@@ -52,13 +56,14 @@ PROGRAM RADIATION
       READ(20,*) Dati(Riga, :)
    END DO
 
-   !PROCESSING DATI
-   DataBuffer(1:6) = INT(Dati(1, 1:6))
+ !-----------------------------------------------------------------------------------------------------------------------  
+ !PROCESSING DATI
+   DataBuffer(1:6) = INT(Dati(1, 1:6)) !SALVO PRIMO GIORNO
    Riga = 1
    DO Riga = 1, NumeroRighe
 
       !CONTROLLO SE E' CAMBIATO IL GIORNO
-      DataCheck = .TRUE.  ! Initialize DataCheck to TRUE
+      DataCheck = .TRUE.
       i=1
       DO i = 1, 3
          IF (INT(Dati(Riga, i)) /= DataBuffer(i)) THEN
@@ -67,12 +72,14 @@ PROGRAM RADIATION
          END IF
       END DO
 
-      !SE IL DATO E NULLO INTERPOLO tra i primi due valori non nulli
+    !-----------------------------------------------------------------------------------------------------------------------
+    !INTERPOLAZIONE LINEARE
+    !SE IL DATO E' NULLO INTERPOLO TRA I DUE VALORI NON NULLI PIU VICINI E SOSTITUISCO I VALORI NULLI CON I VALORI 
+      
       i=Riga
       j=Riga
       interpolazione: IF(DataCheck .AND. Dati(Riga,7)<-998.0) THEN
-
-         !CERCO PRIMO VALORE NON NULLO PRECEDENTE
+         !CERCO PRIMO VALORE NON NULLO PRECEDENTE (ANCHE SE IN QUESTO CASO IL PRECEDENTE E' SICURAMENTE NON NULLO)
          DO
             IF(Dati(i,7)>-998.0) EXIT
             i=i-1
@@ -82,51 +89,63 @@ PROGRAM RADIATION
             IF(Dati(j,7)>-998.0) EXIT
             j=j+1
          END DO
-         !SE HO PIU DI 4 VALORI NULLI ESCO
+         !SE HO PIU DI 4 VALORI NULLI ESCI
          IF(j-i>5) THEN
             WRITE(*,*) 'Troppi valori nulli tra', i+1+1, j-1+1 !TENGO CONTO CHE HO UNA RIGA DI INTESTAZIONE PER LETTURA A MANO DA FILE
             STOP
          END IF
-
          !ALTRIMENTI INTERPOLAZIONE LINEARE
          WRITE(*,101) 'Interpolazione tra', INT(Dati(i+1,1:6)), 'e', INT(Dati(j-1,1:6))
-         m = (Dati(j,7)-Dati(i,7))/(j-i)
-         q = Dati(i,7)-m*i
-         k=i+1
+         m = (Dati(j,7)-Dati(i,7))/(j-i) !COEFFICIENTE ANGOLARE RETTA
+         q = Dati(i,7)-m*i !INTERCETTA RETTA
+         k=i+1 !TORNO AL PRIMO VALORE NULLO
+         !SOSTITUISCO I VALORI NULLI CON I VALORI INTERPOLATI
          DO
             Dati(k,7) = m*k+q
-            WRITE(*,*) k, Dati(k,7)
+            WRITE(*,'(I4,1X,F10.4)') k, Dati(k,7)
             IF(k==j-1) EXIT
             k=k+1
          END DO
-
       END IF interpolazione
 
+      !FINE INTERPOLAZIONE
+      !-----------------------------------------------------------------------------------------------------------------------
+
+      !SE NON E CAMBIATO IL GIORNO SOMMO I VALORI SHORTWAVE E INFRARED
       IF(DataCheck .AND. Dati(Riga,7)>=0.0) THEN
          TotalShortwave = TotalShortwave + Dati(Riga,7)
       ELSE IF(DataCheck  .AND. Dati(Riga,7)<0.0) THEN
          TotalInfrared = TotalInfrared + Dati(Riga,7)
+
+      !SE E' CAMBIATO IL GIORNO SCRIVO I VALORI E RESETTO LE VARIABILI
       ELSE
-         DataBuffer(1:3) = INT(Dati(Riga,1:3))
+         !CONVERTO IN MJ/m^2
+         TotalShortwave = TotalShortwave * 6e-5
+         TotalInfrared = TotalInfrared * 6e-5
+         DataBuffer(1:3) = INT(Dati(Riga,1:3)) !SALVO NUOVO GIORNO
          WRITE(21,100) DataBuffer(1:3), TotalShortwave, TotalInfrared
          TotalShortwave = 0
          TotalInfrared = 0
       END IF
-
    END DO
 
-!CONVERTO IN MJ/m^2
+   !FINE PROCESSING DATI
+   !-----------------------------------------------------------------------------------------------------------------------
+
+ !CONVERTO IN MJ/m^2
    TotalShortwave = TotalShortwave * 6e-5
    TotalInfrared = TotalInfrared * 6e-5
 
-!SCRITTURA ULTIMO GIORNO
-   WRITE(21,100) DataBuffer(1:3), TotalShortwave, TotalInfrared
+ !SCRITTURA ULTIMO GIORNO
+ WRITE(21,100) DataBuffer(1:3), TotalShortwave, TotalInfrared
+ !CHIUSURA FILE
+ CLOSE(20)
+ CLOSE(21)
+ !FORMATI
+ 100 FORMAT(I2,1X,I2,1X,I4,1X,F10.4,1X,F10.4)
+ 101 FORMAT(A,1X,I2,'/',I2,'/',I4,1X,I2,':',I2,':',I2,1X,A,1X,I2,'/',I2,'/',I4,1X,I2,':',I2,':',I2)
 
-   CLOSE(20)
-   CLOSE(21)
-100 FORMAT(I2,1X,I2,1X,I4,1X,F10.4,1X,F10.4)
-101 FORMAT(A,1X,I2,'/',I2,'/',I4,1X,I2,':',I2,':',I2,1X,A,1X,I2,'/',I2,'/',I4,1X,I2,':',I2,':',I2)
-
-   DEALLOCATE(Dati)
+ !DEALLOCAZIONE
+ DEALLOCATE(Dati)
 
 END PROGRAM RADIATION
--- lez9/compile.sh
+++ lez9/compile.sh
@@ -2,4 +2,4 @@
 # is the character for comments in bash (here we choose the bash interpreter to be used)
 
 # program compilation and linking
-gfortran -Wextra -Wall -Wconversion -fimplicit-none -pedantic -fcheck=all -fbacktrace -o duesensori2.f90 duesensori2.f90
+gfortran -Wextra -Wall -Wconversion -fimplicit-none -pedantic -fcheck=all -fbacktrace -o precision.exe precision.F90
