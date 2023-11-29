6f3d306310a0f78b5cf56a09d7834cf82367f063
Mattia Robbiano <120450443+mattia-robbiano@users.noreply.github.com>
2023-11-29 09:34:57 +0000

    Commenti

--- consegna3/MATH.f90
+++ consegna3/MATH.f90
@@ -1,6 +1,6 @@
 MODULE MATH
 CONTAINS
-   !This subroutine convert a sexagesimal angle to radians
+   !SUBROUTINE PER CONVERTIRE DA SESSAGGIMALE A RADIANTI
    SUBROUTINE SEXA2RAD(AngleDeg,Radians)
       IMPLICIT NONE
       REAL, INTENT(IN) :: AngleDeg
@@ -9,7 +9,7 @@ CONTAINS
       Radians = (REAL(INT(AngleDeg))*PI/180)+(AngleDeg-REAL(INT(AngleDeg)))
    END SUBROUTINE SEXA2RAD
 
-   !This subroutine rotates a vector by the angles Phi, Theta, and Psi, where the angles are in sexagesimal degrees.
+   !SUBROUTINE PER RUOTARE (x,y,z) DI (Phi,Theta,Psi) ANGOLI DI EULERO SESSAGESIMALI RESTITUISCE (xRot,yRot,zRot).
    SUBROUTINE VECTOR_ROTATION(x,y,z,xRot,yRot,zRot,PhiDeg,ThetaDeg,PsiDeg,VERBOSE)
       IMPLICIT NONE
       REAL, INTENT(IN) :: PhiDeg,ThetaDeg,PsiDeg, x,y,z
@@ -18,15 +18,15 @@ CONTAINS
       REAL :: Phi,Theta,Psi
       REAL, DIMENSION(3) :: Input,Output
       REAL, DIMENSION(3,3) :: RotationMatrix
-      !Convert the input vector to a matrix
+      !CONVERTO INPUT IN UN ARRAY
       Input(1) = x
       Input(2) = y
       Input(3) = z
-      !Convert from sexagesimal to radians
+      !CONVERTO A RADIANTI
       CALL SEXA2RAD(PhiDeg,Phi)
       CALL SEXA2RAD(ThetaDeg,Theta)
       CALL SEXA2RAD(PsiDeg,Psi)
-      !Calculate the rotation matrix
+      !MATRICE DI ROTAZIONE
       RotationMatrix(1,1) = COS(Psi)*COS(Phi)-COS(Theta)*SIN(Phi)*SIN(Psi)
       RotationMatrix(1,2) = -SIN(Psi)*COS(Phi)-COS(Theta)*SIN(Phi)*COS(Psi)
       RotationMatrix(1,3) = SIN(Theta)*SIN(Phi)
@@ -36,20 +36,18 @@ CONTAINS
       RotationMatrix(3,1) = SIN(Psi)*SIN(Theta)
       RotationMatrix(3,2) = COS(Psi)*SIN(Theta)
       RotationMatrix(3,3) = COS(Theta)
-      !Rotate the vector
+      !RUOTO VETTORE
       Output = MATMUL(RotationMatrix,Input)
-      !Return the rotated vector
+      !RITORNO VETTORE RUOTATO
       xRot = Output(1)
       yRot = Output(2)
       zRot = Output(3)
 
-      !Return the rotated vector if VERBOSE
+      !SE VERBOSE STAMPO ANGOLI E MATRICE DI ROTAZIONE
       IF(PRESENT(VERBOSE).AND. VERBOSE) THEN
-         !Given Rotation angles in right format:
          WRITE(*,*) 'Rotation Angles'
          WRITE(*,'(3F10.5)') PhiDeg,ThetaDeg,PsiDeg
          WRITE(*,*)
-         !Print the rotation matrix in right format
          WRITE(*,*) 'Rotation Matrix'
          WRITE(*,'(3F10.5)') RotationMatrix(1,:)
          WRITE(*,'(3F10.5)') RotationMatrix(2,:)
